fad3a7c26c372f52f79222044ae924efd7e13d46
Lorenzo Bergami <lorebergam@gmail.com>
2025-02-08 21:03:38 +0100

    refined mqtt and serial agent

--- control-unit-backend/project/src/main/java/com/cub/MqttAgentVerticle.java
+++ control-unit-backend/project/src/main/java/com/cub/MqttAgentVerticle.java
@@ -1,29 +1,62 @@
 package com.cub;
 
+import io.netty.handler.codec.mqtt.MqttQoS;
 // In your MQTT agent Verticle
 import io.vertx.core.AbstractVerticle;
 import io.vertx.mqtt.MqttClient;
+import io.vertx.core.buffer.Buffer;
+import com.cub.constants.EventBusAddress;
 
 public class MqttAgentVerticle extends AbstractVerticle {
 
+    private static final String BROKER = "broker.mqtt-dashboard.com";
+    private static final int MQTT_PORT = 1883;
+    private final MqttClient client;
+    public static final String TEMP_TOPIC = "LAWRENC0-STM/temperature";
+    public static final String FREQ_TOPIC = "LAWRENC0-STM/frequency";
+
+    public MqttAgentVerticle() {
+        this.client = MqttClient.create(vertx);
+    }
+
     @Override
     public void start() {
-        MqttClient client = MqttClient.create(vertx);
-        client.connect(1883, "mqtt-broker-address", s -> {
+        client.connect(MQTT_PORT, BROKER, s -> {
             if (s.succeeded()) {
                 System.out.println("Connected to MQTT broker");
-                client.subscribe("temperature/sensor", 0);
+                client.subscribe(TEMP_TOPIC, 0);
+                client.subscribe(FREQ_TOPIC, 0);
             } else {
                 System.err.println("Failed to connect to MQTT broker");
             }
         });
 
         client.publishHandler(message -> {
-            if ("temperature/sensor".equals(message.topicName())) {
+            if (TEMP_TOPIC.equals(message.topicName())) {
                 double temperature = Double.parseDouble(message.payload().toString());
                 // Publish the temperature update to the system manager
-                vertx.eventBus().publish("temperature.updates", temperature);
+                vertx.eventBus().publish(EventBusAddress.TEMP_ADDRESS.getAddress(), temperature);
             }
         });
+    }
+
+    public void sendFreqUpdate(String message) {
+        if (client != null && client.isConnected()) {
+            client.publish(
+                    FREQ_TOPIC,
+                    Buffer.buffer(message),
+                    MqttQoS.AT_LEAST_ONCE,
+                    false,
+                    false,
+                    ack -> {
+                        if (ack.succeeded()) {
+                            System.out.println("Message published to topic: " + FREQ_TOPIC);
+                        } else {
+                            System.out.println("Failed to publish message: " + ack.cause().getMessage());
+                        }
+                    });
+        } else {
+            System.out.println("MQTT client is not connected!");
+        }
     }
 }
--- control-unit-backend/project/src/main/java/com/cub/SerialAgentVerticle.java
+++ control-unit-backend/project/src/main/java/com/cub/SerialAgentVerticle.java
@@ -2,30 +2,23 @@ package com.cub;
 
 import com.fazecast.jSerialComm.SerialPort;
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.json.JsonObject;
+import com.cub.constants.EventBusAddress;
 
 public class SerialAgentVerticle extends AbstractVerticle {
 
+    private static final int BAUD_RATE = 115200;
+    private static final String COMM_PORT = "COM8";
     private SerialPort serialPort;
 
     @Override
     public void start() {
         // Open your serial port (adjust port name and settings as needed)
-        serialPort = SerialPort.getCommPort("COM3");
-        serialPort.setBaudRate(9600);
+        serialPort = SerialPort.getCommPort(COMM_PORT);
+        serialPort.setBaudRate(BAUD_RATE);
         if (!serialPort.openPort()) {
             System.err.println("Failed to open serial port");
             return;
         }
-
-        // Listen for commands from the system manager
-        vertx.eventBus().consumer("window.controller.commands", message -> {
-            JsonObject command = (JsonObject) message.body();
-            System.out.println("Received serial command: " + command);
-            // Send command to the hardware via the serial port
-            String commandString = command.encode();
-            serialPort.writeBytes(commandString.getBytes(), commandString.length());
-        });
 
         // Optionally, read data from the serial port periodically
         vertx.setPeriodic(1000, id -> {
@@ -34,7 +27,8 @@ public class SerialAgentVerticle extends AbstractVerticle {
                 serialPort.readBytes(buffer, buffer.length);
                 String serialResponse = new String(buffer);
                 // Publish serial data for further processing
-                vertx.eventBus().publish("serial.data", new JsonObject().put("response", serialResponse));
+                vertx.eventBus().publish(EventBusAddress.WCU_ADDRESS.getAddress(),
+                        serialResponse);
             }
         });
     }
@@ -44,5 +38,11 @@ public class SerialAgentVerticle extends AbstractVerticle {
         if (serialPort != null && serialPort.isOpen()) {
             serialPort.closePort();
         }
+    }
+
+    public void sendMessage(String message) {
+        System.out.println("Received serial message to be sent: " + message);
+        // Send command to the hardware via the serial port
+        serialPort.writeBytes(message.getBytes(), message.length());
     }
 }
