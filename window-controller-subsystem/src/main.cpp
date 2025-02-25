72bdf07851841479bce760aefd6aa548afafe1ee
Lorenzo Bergami <lorebergam@gmail.com>
2025-02-25 15:41:06 +0100

    removed unused Lines

--- window-controller-subsystem/src/main.cpp
+++ window-controller-subsystem/src/main.cpp
@@ -88,8 +88,6 @@ void loop() {
     if (msg != NULL) {
         receivedMsg = msg->getContent();
         delete msg;
-        // lcd->clearScreen();t
-        // lcd->display((String)receivedMsg, 0);
         if (Pattern::matchTemp(receivedMsg)) {
             temp = Pattern::getTemp(receivedMsg);
         } else if (Pattern::matchAngle(receivedMsg)) {
